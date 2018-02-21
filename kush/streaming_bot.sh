abdef6e39c3ac9ee2011dd93223e4c8281a30c50
Kushagra Singhal <singhalkushagra@gmail.com>
2018-02-21 22:22:13 +0100

    Run streaming from the directory it's in

--- kush/streaming_bot.sh
+++ kush/streaming_bot.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 source $HOME/.bashrc 
-nohup $HOME/miniconda3/bin/python /home/kush/FairFrog_Code/APICode/Twitter/kush/fairfrog_stream.py &> /home/kush/Logs/nohup.out &
+cd $(dirname $0)
+nohup $HOME/miniconda3/bin/python fairfrog_stream.py &> /home/kush/Logs/nohup.out &
 stream_pid=$!
 echo "Will kill $stream_pid tonight at 23:30"
 echo "kill -15 $stream_pid" | at 23:30
