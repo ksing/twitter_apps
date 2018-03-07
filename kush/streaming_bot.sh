d6933b839c08aaeaa2ea49e611cbfbb2dbe29b34
Kushagra Singhal <kush@fairfrogdev.nl>
2018-03-07 17:53:15 +0100

    streaming_bot never stops

--- kush/streaming_bot.sh
+++ kush/streaming_bot.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 source $HOME/.bashrc 
 cd $(dirname $0)
-nohup $HOME/miniconda3/bin/python fairfrog_stream.py &> /home/kush/Logs/nohup.out &
-stream_pid=$!
-echo "Will kill $stream_pid tonight at 23:30"
-echo "kill -15 $stream_pid" | at 23:30
+if [[ $(ps -f -u kush | grep fairfrog_stream | wc -l) -lt 2 ]]; then
+	$HOME/miniconda3/bin/python fairfrog_stream.py &
+fi
